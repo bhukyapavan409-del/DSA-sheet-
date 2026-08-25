e01bafa21c1a9fd45c47f6838d65f6f2f3866584
PAVAN KUMAR <bhukyapavan409@gmail.com>
2026-08-25 16:53:54 +0530

    Time: 0 ms (100%), Space: 27.4 MB (16.6%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
 
 
-    vector<int> getLeftMax(vector<int>&height,int &n){
+    vector<int> getLeftMax(vector<int>&height,int &n){   // max element till that index 
         vector<int>prefix(n);
-        prefix[0]=height[0];
+        prefix[0]=height[0];           
         for(int i=1;i<n;i++){
             prefix[i]=max(prefix[i-1],height[i]);
         }
         return prefix;
     }
-   vector<int> getRightMax(vector<int>&height,int &n){
+   vector<int> getRightMax(vector<int>&height,int &n){    // max element till the index
     vector<int>suffix(n);
-    suffix[n-1]=height[n-1];
+    suffix[n-1]=height[n-1];    // it comes from the back side 
     for(int i=n-2;i>=0;i--){
         suffix[i]=max(suffix[i+1],height[i]);
     }
