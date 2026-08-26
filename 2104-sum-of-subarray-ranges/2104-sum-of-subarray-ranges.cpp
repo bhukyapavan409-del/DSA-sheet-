a9e3afe42726d49777f3d50fcdce68c42ddeebe4
PAVAN KUMAR <bhukyapavan409@gmail.com>
2026-08-26 23:37:57 +0530

    Time: 17 ms (15.63%), Space: 13.8 MB (97.57%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
+        // brute force
         int n=nums.size();
         long long sum=0;
         for(int i=0;i<n;i++){
