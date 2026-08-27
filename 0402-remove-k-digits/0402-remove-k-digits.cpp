3d3bc6cf2ea8aebf0d71ed4d0296b7a0e0fa041d
PAVAN KUMAR <bhukyapavan409@gmail.com>
2026-08-27 13:03:58 +0530

    Time: 0 ms (100%), Space: 11.5 MB (47.73%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int n=num.length();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && k>0 &&(st.top()-'0')>(num[i]-'0')){
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+    }
+    while(k>0 && !st.empty()){   // remove from the end 
+    st.pop();
+    k--;
+    }
+    if(st.empty()) return "0";
+    string res="";
+    while(!st.empty()){   //stack gives the value in reverse order 
+        res+=st.top();
+        st.pop();
+    }
+        while(res.size()!=0 &&res.back()=='0')   // it removes the leading zeroes 
+        res.pop_back();
+        reverse(res.begin(),res.end());
+        if(res.empty()) return "0";
+        return res;
+    }
+};
