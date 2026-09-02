cb0859f3e997c3de9dbe78efe2ec0f76ddfc021c
PAVAN KUMAR <bhukyapavan409@gmail.com>
2026-09-02 22:22:00 +0530

    Time: 0 ms (100%), Space: 8.2 MB (48.64%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,7 +3,7 @@ public:
     int hammingWeight(int n) {
         int cnt=0;
         while(n!=0){
-            n=n&(n-1);
+            n=n&(n-1);  // it removes the 1 in every iteration 
             cnt++;
         }
      return cnt;   
